0eb1e3f9aa0c2052620730a540c0f7cc44b78b67
Jacob.Mercado <jacob.mercado@jalasoft.com>
2022-01-29 19:01:38 -0400

    create imte controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers {
+    public class ItemController : Controller {
+        private readonly ApplicationDbContext _context;
+        
+        public ItemController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index() {
+            return View("Index", _context.Items);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item) {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
