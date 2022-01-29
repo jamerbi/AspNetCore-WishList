62c7f1964929179ac2d5028f30c4fafc4314c2a4
Jacob.Mercado <jacob.mercado@jalasoft.com>
2022-01-29 18:00:16 -0400

    update create item model

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,7 +3,9 @@ using WishList.Models;
 
 namespace WishList.Data {
     public class ApplicationDbContext : DbContext {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions options) : base(options) {
+            
+        }
         public DbSet<Item> Items { get; set; }
     }
 }
