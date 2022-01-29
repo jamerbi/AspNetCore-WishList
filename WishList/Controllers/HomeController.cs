0359565979fae008b5b000b1aa240f0a3bed1e22
Jacob.Mercado <jacob.mercado@jalasoft.com>
2022-01-29 17:39:37 -0400

    create home and error view

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList 
+{
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Error() {
+            return View();
+        }
+    }
+}
